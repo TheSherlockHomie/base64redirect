0e5b8c801a95c9a9c74c8e02a69100c4b3af9c99
Kushagra Kalash <kushagrakdev@gmail.com>
2021-10-24 01:27:50 +0530

    initial commit

---
+++ src/main/java/com/thesherlockhomie/base64redirect/Base64redirectApplication.java
@@ -0,0 +1,13 @@
+package com.thesherlockhomie.base64redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64redirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Base64redirectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thesherlockhomie/base64redirect/Base64redirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesherlockhomie.base64redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64redirectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
